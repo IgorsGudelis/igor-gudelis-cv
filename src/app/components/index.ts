b44d7d3f4732d2f05c1a97a46b9298114169ab5d
Igors <igors.gudelis@gmail.com>
2021-04-01 23:57:00 +0300

    feat: init about view

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,3 +1,4 @@
+import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
-export const APP_COMPONENTS = [HomeComponent];
+export const APP_COMPONENTS = [AboutComponent, HomeComponent];
