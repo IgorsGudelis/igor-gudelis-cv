af29b4b6bf80762b69a5e6e1d22416199ffb8931
Igors <igors.gudelis@gmail.com>
2021-06-18 19:38:57 +0300

    fix: add dialog image placeholder

--- src/app/shared/services/dialog.service.ts
+++ src/app/shared/services/dialog.service.ts
@@ -1,4 +1,10 @@
-import { ComponentRef, Injectable, Type } from '@angular/core';
+import {
+  ComponentRef,
+  Injectable,
+  Renderer2,
+  RendererFactory2,
+  Type
+} from '@angular/core';
 import { Subscription } from 'rxjs';
 
 import { ComponentFactoryService } from './component-factory.service';
@@ -9,12 +15,18 @@ import { ComponentFactoryService } from './component-factory.service';
 export class DialogService {
   private dialogRef!: ComponentRef<any> | null;
   private dialogCloseSub!: Subscription;
+  private renderer!: Renderer2;
 
   get dialogInstance(): any {
     return this.dialogRef?.instance;
   }
 
-  constructor(private componentFactoryService: ComponentFactoryService) {}
+  constructor(
+    private componentFactoryService: ComponentFactoryService,
+    private rendererFactory: RendererFactory2
+  ) {
+    this.renderer = this.rendererFactory.createRenderer(null, null);
+  }
 
   closeDialog(): void {
     this.destroyDialogSources();
@@ -22,10 +34,14 @@ export class DialogService {
 
   openDialog<T>(componentType: Type<T>): T | void {
     if (!this.dialogRef) {
+      const bodyElem = document.querySelector('body');
+
+      this.renderer.setStyle(bodyElem, 'overflow', 'hidden');
       this.dialogRef = this.componentFactoryService.create(componentType);
       this.dialogCloseSub?.unsubscribe();
       this.dialogCloseSub = this.dialogInstance.afterClose$?.subscribe(() => {
         this.destroyDialogSources();
+        this.renderer.setStyle(bodyElem, 'overflow', 'auto');
       });
 
       return this.dialogInstance;
