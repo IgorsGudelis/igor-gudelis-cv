7eafc6079d56a8a6a2459fa4c17ad19185ef999d
Igors <igors.gudelis@gmail.com>
2021-06-29 22:05:25 +0300

    feat: add service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { CoreModule } from '@core/core.module';
 import { SharedModule } from '@shared/shared.module';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { APP_COMPONENTS } from './components';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreModule,
+    SharedModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   declarations: [AppComponent, ...APP_COMPONENTS],
   providers: [],
   bootstrap: [AppComponent],
